3f6178f019899e882ed9f792b10cb59095a8889a
AncuL001 <ancul001@gmail.com>
2023-10-03 23:44:00 +0700

    fix functions to accept all kinds of S B C settings

--- yolov1_impl/utils.py
+++ yolov1_impl/utils.py
@@ -240,6 +240,7 @@ def get_bboxes(
     pred_format="cells",
     box_format="midpoint",
     device="cuda",
+    S=7, B=2, C=20
 ):
     all_pred_boxes = []
     all_true_boxes = []
@@ -256,8 +257,8 @@ def get_bboxes(
             predictions = model(x)
 
         batch_size = x.shape[0]
-        true_bboxes = cellboxes_to_boxes(labels)
-        bboxes = cellboxes_to_boxes(predictions)
+        true_bboxes = cellboxes_to_boxes(labels, S, B, C)
+        bboxes = cellboxes_to_boxes(predictions, S, B, C)
 
         for idx in range(batch_size):
             nms_boxes = non_max_suppression(
@@ -287,7 +288,7 @@ def get_bboxes(
 
 
 
-def convert_cellboxes(predictions, S=7):
+def convert_cellboxes(predictions, S=7, B=2, C=20):
     """
     Converts bounding boxes output from Yolo with
     an image split size of S into entire image ratios
@@ -300,21 +301,21 @@ def convert_cellboxes(predictions, S=7):
 
     predictions = predictions.to("cpu")
     batch_size = predictions.shape[0]
-    predictions = predictions.reshape(batch_size, 7, 7, 30)
-    bboxes1 = predictions[..., 21:25]
-    bboxes2 = predictions[..., 26:30]
+    predictions = predictions.reshape(batch_size, S, S, C + (5 * B))
+    bboxes1 = predictions[..., C+1:C+5]
+    bboxes2 = predictions[..., C+6:C+10]
     scores = torch.cat(
-        (predictions[..., 20].unsqueeze(0), predictions[..., 25].unsqueeze(0)), dim=0
+        (predictions[..., C].unsqueeze(0), predictions[..., C+5].unsqueeze(0)), dim=0
     )
     best_box = scores.argmax(0).unsqueeze(-1)
     best_boxes = bboxes1 * (1 - best_box) + best_box * bboxes2
-    cell_indices = torch.arange(7).repeat(batch_size, 7, 1).unsqueeze(-1)
+    cell_indices = torch.arange(S).repeat(batch_size, S, 1).unsqueeze(-1)
     x = 1 / S * (best_boxes[..., :1] + cell_indices)
     y = 1 / S * (best_boxes[..., 1:2] + cell_indices.permute(0, 2, 1, 3))
     w_y = 1 / S * best_boxes[..., 2:4]
     converted_bboxes = torch.cat((x, y, w_y), dim=-1)
-    predicted_class = predictions[..., :20].argmax(-1).unsqueeze(-1)
-    best_confidence = torch.max(predictions[..., 20], predictions[..., 25]).unsqueeze(
+    predicted_class = predictions[..., :C].argmax(-1).unsqueeze(-1)
+    best_confidence = torch.max(predictions[..., C], predictions[..., C+5]).unsqueeze(
         -1
     )
     converted_preds = torch.cat(
@@ -324,8 +325,8 @@ def convert_cellboxes(predictions, S=7):
     return converted_preds
 
 
-def cellboxes_to_boxes(out, S=7):
-    converted_pred = convert_cellboxes(out).reshape(out.shape[0], S * S, -1)
+def cellboxes_to_boxes(out, S=7, B=2, C=20):
+    converted_pred = convert_cellboxes(out, S, B, C).reshape(out.shape[0], S * S, -1)
     converted_pred[..., 0] = converted_pred[..., 0].long()
     all_bboxes = []
 
