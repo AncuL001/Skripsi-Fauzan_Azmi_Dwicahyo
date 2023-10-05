a08833d442d1a53175c0ba0ed469ac6681b19b4c
AncuL001 <ancul001@gmail.com>
2023-10-05 12:29:08 +0700

    add full taco training code

---
+++ yolov1_impl/train_taco.py
@@ -0,0 +1,108 @@
+import torch
+import torchvision.transforms as transforms
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from model import YoloV1
+from taco_dataset import CoCoDatasetForYOLO
+from loss import YoloLoss
+from utils import (
+    mean_average_precision,
+    get_bboxes,
+)
+
+LEARNING_RATE = 2e-5
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16 # 64 in original paper but I don't have that much vram, grad accum?
+WEIGHT_DECAY = 0
+EPOCHS = 10
+NUM_WORKERS = 0
+PIN_MEMORY = True
+LOAD_MODEL = False
+LOAD_MODEL_FILE = "overfit.pth.tar"
+
+DATASET_PATH = '../downloads/TACO/data'
+anns_file_path = DATASET_PATH + '/' + 'annotations.json'
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, bboxes):
+        for t in self.transforms:
+            img, bboxes = t(img), bboxes
+
+        return img, bboxes
+
+
+transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
+
+model = YoloV1(split_size=7, num_boxes=2, num_classes=1).to(DEVICE)
+optimizer = optim.Adam(
+    model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
+)
+loss_fn = YoloLoss(S=7, B=2, C=1)
+
+dataset = CoCoDatasetForYOLO(
+    root=DATASET_PATH,
+    annFile=anns_file_path,
+    transform=transform,
+    C=1
+)
+
+# # for testing with a small dataset
+# training_portion = list(range(0, 32))
+# testing_portion = list(range(32, 64))
+# train_dataset = torch.utils.data.Subset(dataset, training_portion)
+# test_dataset = torch.utils.data.Subset(dataset, testing_portion)
+
+train_percentage = 0.8
+train_size = int(train_percentage * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=BATCH_SIZE,
+    num_workers=NUM_WORKERS,
+    pin_memory=PIN_MEMORY,
+    shuffle=True,
+    drop_last=True,
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=BATCH_SIZE,
+    num_workers=NUM_WORKERS,
+    pin_memory=PIN_MEMORY,
+    shuffle=True,
+    drop_last=True,
+)
+
+for epoch in range(EPOCHS):
+    pred_boxes, target_boxes = get_bboxes(
+        train_loader, model, iou_threshold=0.5, threshold=0.4, device=DEVICE,
+        S=7, B=2, C=1
+    )
+
+    mean_avg_prec = mean_average_precision(
+        pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
+    )
+    print(f"Train mAP: {mean_avg_prec}")
+
+    loop = tqdm(train_loader, leave=True)
+    mean_loss = []
+
+    for batch_idx, (x, y) in enumerate(loop):
+        x, y = x.to(DEVICE), y.to(DEVICE)
+        out = model(x)
+        loss = loss_fn(out, y)
+        mean_loss.append(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # update progress bar
+        loop.set_postfix(loss=loss.item())
+
+    print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
